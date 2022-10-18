39424464a046245f5b884262829673ac7c200d68
Philip <krenity@gmail.com>
2022-10-18 22:45:45 +0100

    adasd

--- main_testing.py
+++ main_testing.py
@@ -20,12 +20,12 @@ upgrademult4 = 1
 upgrademult5 = 1
 upgrademult6 = 1
 #---------------------------------------# 
-upgradeprice1 = (100*upgrademult1)
-upgradeprice2 = (1000*upgrademult2)
-upgradeprice3 = (5000*upgrademult3)
-upgradeprice4 = (1000*upgrademult4)
-upgradeprice5 = (25000*upgrademult5)
-upgradeprice6 = (60000*upgrademult6)
+upgradeprice1 = (50*upgrademult1)
+upgradeprice2 = (300*upgrademult2)
+upgradeprice3 = (1000*upgrademult3)
+upgradeprice4 = (1500*upgrademult4)
+upgradeprice5 = (3000*upgrademult5)
+upgradeprice6 = (10000*upgrademult6)
 ####################################### Clicker upgrades
 clickermult1 = 1
 clickermult2 = 1
@@ -284,11 +284,7 @@ def start_menu():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pass
     
-    screen.fill(dark_grey)
-    
-print("ooooooooo")
-    
-        
+    screen.fill(dark_grey) 
         
 #Main loop
 def main_loop():
@@ -307,24 +303,24 @@ def main_loop():
             
                 elif Upgrade1 <= mouse_pos[0] <= (Upgrade1+sizex_1) and starty_1 <= mouse_pos[1] <= starty_1+sizey_1:
                     upgrade1()
-                    upgradeprice1 = (100*upgrademult1)
+                    upgradeprice1 = (50*upgrademult1)
                 elif Upgrade2 <= mouse_pos[0] <= (Upgrade2+sizex_2) and starty_2 <= mouse_pos[1] <= starty_2+sizey_2:
                     upgrade2()
                     upgradeprice2 = (1000*upgrademult2)
                 elif Upgrade3 <= mouse_pos[0] <= (Upgrade3+sizex_3) and starty_3 <= mouse_pos[1] <= starty_3+sizey_3:
                     upgrade3()
-                    upgradeprice3 = (5000*upgrademult3)
+                    upgradeprice3 = (1500*upgrademult3)
                 elif Upgrade4 <= mouse_pos[0] <= (Upgrade4+sizex_4) and starty_4 <= mouse_pos[1] <= starty_4+sizey_4:
                     upgrade4()
-                    upgradeprice4 = (1000*upgrademult4)
+                    upgradeprice4 = (3000*upgrademult4)
                 elif Upgrade5 <= mouse_pos[0] <= (Upgrade5+sizex_5) and starty_5 <= mouse_pos[1] <= starty_5+sizey_5:
                     upgrade5()
-                    upgradeprice5 = (25000*upgrademult5)
+                    upgradeprice5 = (10000*upgrademult5)
                 elif Upgrade6 <= mouse_pos[0] <= (Upgrade6+sizex_6) and starty_6 <= mouse_pos[1] <= starty_6+sizey_6:
                     upgrade6()
                     upgradeprice6 = (60000*upgrademult6)
             
-                
+                upgradeprice1 = (50*upgrademult1)
         global printer 
         screen.blit(background,(0,0)) 
         mouse_pos = pygame.mouse.get_pos()
