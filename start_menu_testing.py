74bb3c230d01c413a9708db89ba1b66fdb7683e9
BzZ7t <silyirishman1983@gmail.com>
2022-10-19 11:52:53 +0100

    Counter_num = _ was changed to 0

--- start_menu_testing.py
+++ start_menu_testing.py
@@ -7,7 +7,7 @@ import sys , os
 import time
 import random
 ####################################### main storage
-Counter_num = 
+Counter_num = 0
 Counter_auto = 0    
 Counter_click = 1
 Counter_mult = 1.0
