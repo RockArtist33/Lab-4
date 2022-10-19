9ecaf2b7b3ad2c3083d357eadcf234e6ff4878ac
BzZ7t <silyirishman1983@gmail.com>
2022-10-19 15:53:49 +0100

    fixed

--- main_testing.py
+++ main_testing.py
@@ -521,7 +521,7 @@ def main_loop():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if startx <= mouse_pos[0] <= (startx+sizex) and starty <= mouse_pos[1] <= (starty+sizey):
                     Counter_num = Counter_num + Counter_click
-                    play_audio(ding, 0)
+                    play_audio(print_audio,random.randint(0,2))
                 if Upgrade1 <= mouse_pos[0] <= (Upgrade1+sizex_1) and starty_1 <= mouse_pos[1] <= starty_1+sizey_1:
                     cant1 = upgrade1()
                     start_time = pygame.time.get_ticks()
